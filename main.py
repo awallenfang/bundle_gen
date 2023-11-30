Fixed basic input rotation

main.py
@@ -2,7 +2,7 @@ import mitsuba as mi
 import numpy as np
 import matplotlib.pyplot as plt
 import drjit as dr
-
+import scipy.stats.qmc as qmc
 
 import fiber
 from brdf import TabulatedBCRDF
@@ -14,18 +14,28 @@ TEST = False
 THETA_TABLE_SIZE=450
 PHI_TABLE_SIZE=880
 
+FIBER_RADIUS = 2.
+BUNDLE_RADIUS = 100.
+
 mi.set_variant('llvm_ad_rgb')
 
 # Get the input rotation to make a difference
-# Input offsets
 # Output offsets in fiber
 
+# Poisson disk sampling in circle
+poisson_engine = qmc.PoissonDisk(d=2, radius=(2.*FIBER_RADIUS)/BUNDLE_RADIUS)
+samples = poisson_engine.random(100000)
+samples -= 0.5
+samples *= 2.*BUNDLE_RADIUS
+samples = samples[np.sqrt(samples[:,0]*samples[:,0] + samples[:,1] * samples[:,1]) <= BUNDLE_RADIUS]
+
+plt.scatter(samples[:,0], samples[:,1])
+plt.show()
+
 fibers = []
-# for x in range(0,105, 5):
-#     for y in range(0,105,5):
-#         fibers.append(fiber.Fiber(x,y,2,[0.,0.,1.]))
-fibers.append(fiber.Fiber(0,0,2,[0.,0.,1.]))
-fibers.append(fiber.Fiber(5,0,2,[0.,0.,1.]))
+for elem in samples:
+    fibers.append(fiber.Fiber(elem[0], elem[1], FIBER_RADIUS, [0.,0.,1.]))
+
 
 radius, center_x, center_y = fiber.get_bounds(fibers)
 
@@ -57,15 +67,17 @@ if not TEST:
     # brdf.set_test_intensities(intensities)
     # brdf.show_layers()
 
-    in_dir = mi.Vector3f(1.,0.,0.)
+    in_dir = mi.Vector3f(1.,1.,0.)
 
     in_pos = mi.Point3f(center_x, center_y, 0.) - dr.normalize(in_dir) * (radius * 1.1)
 
-    renderer = Renderer(fibers, brdf, samples=10, bounces=1000000, in_dir=in_dir, in_pos=in_pos, spread_amt=10)
+    renderer = Renderer(fibers, brdf, samples=1000000, bounces=1000, in_dir=in_dir, in_pos=in_pos)
 
-    out_model = renderer.render_structure()
+    out_model: np.array = renderer.render_structure()
 
     plot_results(out_model)
+
+    out_model.tofile("test")
 
 else:
     rend_scene = fiber.preview_render_dict_from_fibers(fibers)